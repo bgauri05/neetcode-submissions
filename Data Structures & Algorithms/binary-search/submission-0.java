99f605a2b0258d64110777397660665fddf521fc
bgauri05 <gauri.bhonsle@somaiya.edu>
2026-06-25 10:10:44 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int h = nums.length - 1;
+
+        while (l <= h) {
+            int mid = l + (h - l) / 2; // avoids integer overflow
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (target < nums[mid]) {
+                h = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return -1; // target not found
+    }
+}
